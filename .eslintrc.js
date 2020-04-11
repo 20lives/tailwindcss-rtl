91ccceb50a5134406cccbc579772076216a7a8f3
Lior Haim <lior@dotcore.co.il>
2020-04-11 08:10:01 +0000

    Adding ESLint and prettier
    Signed-off-by: Lior Haim <lior@dotcore.co.il>

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "prettier",
+  ],
+  "plugins": [
+    "prettier",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "prettier/prettier": ["error"],
+    "indent": [
+      "error",
+      2 
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  },
+};
