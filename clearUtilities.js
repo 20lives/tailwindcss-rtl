90091177150e552e0899de8213125a1d63aba537
Lior Haim <lior@dotcore.co.il>
2020-07-09 23:58:09 +0300

    minor cleanup

--- clearUtilities.js
+++ clearUtilities.js
@@ -1,10 +1,8 @@
-const utilities = () => {
-  return {
-    '[dir="ltr"] .clear-start': { clear: 'left' },
-    '[dir="ltr"] .clear-end': { clear: 'right' },
-    '[dir="rtl"] .clear-start': { clear: 'right' },
-    '[dir="rtl"] .clear-end': { clear: 'left' },
-  };
-};
+const utilities = () => ({
+  '[dir="ltr"] .clear-start': { clear: 'left' },
+  '[dir="ltr"] .clear-end': { clear: 'right' },
+  '[dir="rtl"] .clear-start': { clear: 'right' },
+  '[dir="rtl"] .clear-end': { clear: 'left' },
+});
 
 module.exports = utilities;
