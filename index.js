c8bde93f1823de3cd07b8ac0549600d72d1438fb
Lior Haim <lior@dotcore.co.il>
2020-07-12 20:48:41 +0300

    Adding target argument

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const borderWidthUtilities = require('./borderWidthUtilities');
 const textAlignUtilities = require('./textAlignUtilities');
 const transformOriginUtilities = require('./transformOriginUtilities');
 
-module.exports = plugin(function ({ addUtilities, e, theme, variants }) {
+module.exports = plugin(function ({ addUtilities, e, theme, variants, target }) {
   addUtilities(paddingUtilities(theme, e), variants('padding'));
   addUtilities(marginUtilities(theme, e), variants('margin'));
   addUtilities(insetUtilities(theme, e), variants('inset'));
