612db7efd09d5bd444258d702a7b2cd3c134167a
Boring Boring <boring@Borings-MacBook-Pro.local>
2022-01-24 15:08:18 +1100

    Fix rtl classes should come after default ones to take the priority

--- src/floatUtilities.js
+++ src/floatUtilities.js
@@ -1,8 +1,8 @@
 const utilities = () => ({
-  '[dir="rtl"] .float-start': { float: 'right' },
-  '[dir="rtl"] .float-end': { float: 'left' },
   ':not([dir="rtl"]) .float-end': { float: 'right' },
   ':not([dir="rtl"]) .float-start': { float: 'left' },
+  '[dir="rtl"] .float-start': { float: 'right' },
+  '[dir="rtl"] .float-end': { float: 'left' },
 });
 
 module.exports = utilities;
--- src/textAlignUtilities.js
+++ src/textAlignUtilities.js
@@ -1,8 +1,8 @@
 const utilities = () => ({
-  '[dir="rtl"] .text-start': { 'text-align': 'right' },
-  '[dir="rtl"] .text-end': { 'text-align': 'left' },
   ':not([dir="rtl"]) .text-end': { 'text-align': 'right' },
   ':not([dir="rtl"]) .text-start': { 'text-align': 'left' },
+  '[dir="rtl"] .text-start': { 'text-align': 'right' },
+  '[dir="rtl"] .text-end': { 'text-align': 'left' },
 });
 
 module.exports = utilities;
